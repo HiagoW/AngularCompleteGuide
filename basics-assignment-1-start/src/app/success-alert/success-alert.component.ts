20d287f55573884dd1c825b8ebc17d3986b67d31
Hiago Petris <Hiago.Petris@ibm.com>
2023-05-22 09:54:40 -0300

    1st Assignment

---
+++ basics-assignment-1-start/src/app/success-alert/success-alert.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-success-alert',
+  template: '<p style="color: green;">success alert works</p>'
+})
+export class SuccessAlertComponent {}
