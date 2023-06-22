8146d3b0fbd898c582e242e7409bb463f02110b4
Hiago Petris <hiago.petris@ibm.com>
2023-06-22 08:59:10 -0300

    Routes CanDeactivate, passing static and dynamic data, resolver

--- routing-start/src/app/app-routing.module.ts
+++ routing-start/src/app/app-routing.module.ts
@@ -8,23 +8,35 @@ import { EditServerComponent } from "./servers/edit-server/edit-server.component
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 import { RouterModule, Routes } from "@angular/router";
 import { AuthGuard } from "./auth-guard.service";
+import { CanDeactivateGuard } from "./servers/edit-server/can-deactivate-guard.service";
+import { ErrorPageComponent } from "./error-page/error-page.component";
+import { ServerResolver } from "./servers/server/server-resolver.service";
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'users', component: UsersComponent, children: [
       { path: ':id/:name', component: UserComponent },
     ] },
-    { path: 'servers', canActivate: [AuthGuard], component: ServersComponent, children: [
-      { path: ':id', component: ServerComponent },
-      { path: ':id/edit', component: EditServerComponent }
+    { 
+      path: 'servers', 
+      // canActivate: [AuthGuard], 
+      canActivateChild: [AuthGuard],
+      component: ServersComponent, 
+      children: [
+      { path: ':id', component: ServerComponent, resolve: {server: ServerResolver} },
+      { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard] }
     ] },
-    { path: 'not-found', component: PageNotFoundComponent },
+    // { path: 'not-found', component: PageNotFoundComponent },
+    { path: 'not-found', component: ErrorPageComponent, data: {message: 'Page not found!'} },
     { path: '**', redirectTo: '/not-found' }
   ];
 
 @NgModule({
     imports: [
-        RouterModule.forRoot(appRoutes)
+      RouterModule.forRoot(appRoutes)
+      // useHash is used when the web server tries to handle the route before reaching Angular
+      // so we add '#' after the server domain so the server will ignore the path and send it to angular
+      // RouterModule.forRoot(appRoutes, {useHash: true})
     ],
     exports: [RouterModule]
 })
