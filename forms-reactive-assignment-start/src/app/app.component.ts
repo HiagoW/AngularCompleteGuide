8838d2721aeb6bb73562511b38d0da78e6074a9d
Hiago Petris <hiago.petris@ibm.com>
2023-07-04 08:40:40 -0300

    Reactive forms assignment

--- forms-reactive-assignment-start/src/app/app.component.ts
+++ forms-reactive-assignment-start/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -13,14 +14,34 @@ export class AppComponent implements OnInit{
 
   ngOnInit(): void {
     this.formGroup = new FormGroup({
-      'name': new FormControl(null, Validators.required),
+      'name': new FormControl(null, [Validators.required], this.usernameValidator),
       'mail': new FormControl(null, [Validators.required, Validators.email]),
-      'projectStatus': new FormControl(null)
+      'projectStatus': new FormControl(this.projectStatus[0])
     })
   }
 
   onSubmit() {
     console.log(this.formGroup.value);
+  }
+
+  // usernameValidator(control: FormControl): {[s:string]: boolean} {
+  //   if (control.value === 'Test') {
+  //     return {'testUsername': true};
+  //   }
+  //   return null;
+  // }
+
+  usernameValidator(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'Test') {
+          resolve({'testUsername': true});
+        } else {
+          resolve(null);
+        }
+      },1500)
+    })
+    return promise;
   }
 
 }
