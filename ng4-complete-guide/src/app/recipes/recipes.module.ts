2a95136364ebfb0205f8bad802f4c402a2588fdf
Hiago Petris <hiago.petris@ibm.com>
2023-07-21 08:58:27 -0300

    Create feature module

---
+++ ng4-complete-guide/src/app/recipes/recipes.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from "@angular/core";
+import { RecipesComponent } from "./recipes.component";
+import { RecipeListComponent } from "./recipe-list/recipe-list.component";
+import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
+import { RecipeItemComponent } from "./recipe-list/recipe-item/recipe-item.component";
+import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
+import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
+import { RouterModule } from "@angular/router";
+import { CommonModule } from "@angular/common";
+import { ReactiveFormsModule } from "@angular/forms";
+
+@NgModule({
+    declarations: [
+        RecipesComponent,
+        RecipeListComponent,
+        RecipeDetailComponent,
+        RecipeItemComponent,
+        RecipeStartComponent,
+        RecipeEditComponent
+    ],
+    // User commonModule for ngFor, ngIf... Since BrowserModule must be imported only once, on app module
+    // Exception: Services can be imported only once. Ex: HttpClientModule provides only services
+    imports: [RouterModule, CommonModule, ReactiveFormsModule],
+    exports: [
+        RecipesComponent,
+        RecipeListComponent,
+        RecipeDetailComponent,
+        RecipeItemComponent,
+        RecipeStartComponent,
+        RecipeEditComponent
+    ]
+})
+export class RecipesModule {}
