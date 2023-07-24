47c5b8d99668d434e806265984044a64166c6c91
Hiago Petris <hiago.petris@ibm.com>
2023-07-24 08:58:39 -0300

    Shared and core modules, lazy loading

--- ng4-complete-guide/src/app/recipes/recipes.module.ts
+++ ng4-complete-guide/src/app/recipes/recipes.module.ts
@@ -5,9 +5,9 @@ import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
 import { RecipeItemComponent } from "./recipe-list/recipe-item/recipe-item.component";
 import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
 import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
-import { RouterModule } from "@angular/router";
-import { CommonModule } from "@angular/common";
 import { ReactiveFormsModule } from "@angular/forms";
+import { RecipesRoutingModule } from "./recipes-routing.module";
+import { SharedModule } from "../shared/shared.module";
 
 @NgModule({
     declarations: [
@@ -20,14 +20,6 @@ import { ReactiveFormsModule } from "@angular/forms";
     ],
     // User commonModule for ngFor, ngIf... Since BrowserModule must be imported only once, on app module
     // Exception: Services can be imported only once. Ex: HttpClientModule provides only services
-    imports: [RouterModule, CommonModule, ReactiveFormsModule],
-    exports: [
-        RecipesComponent,
-        RecipeListComponent,
-        RecipeDetailComponent,
-        RecipeItemComponent,
-        RecipeStartComponent,
-        RecipeEditComponent
-    ]
+    imports: [RecipesRoutingModule, SharedModule, ReactiveFormsModule]
 })
 export class RecipesModule {}
